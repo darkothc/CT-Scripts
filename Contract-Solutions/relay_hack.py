239f776d8b68bc74a56ebd46a29d5a1369f069a2
DarkoTHC <darkothc@yahoo.com>
2026-07-15 02:53:32 -0500

    Orbital Relay Hack solution
    A relay_hack.py solution

---
+++ Contract-Solutions/relay_hack.py
@@ -0,0 +1,25 @@
+log_attempts = 0
+
+code = [0,0,0,0,0,0]
+lock = self.contract.lock
+result = lock.intercept(code)
+
+while True:
+    if log_attempts:
+        print(f"Code: {code} \n Check: {result}")
+
+    if all(result):
+        print(f"---------\n Codefound {code}")
+        break
+
+    for i in range(6):
+        if code[i] >= 100:
+            break
+            
+        if not result[i]:
+            code[i] += 1
+
+
+t = get_component("transmitter")
+t.connect("earth")
+t.transmit(self.contract.id, code)
