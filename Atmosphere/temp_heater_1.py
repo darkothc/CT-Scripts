896eabc4f935044e5d3f2dedc89536d9eb66ac99
DarkoTHC <darkothc@yahoo.com>
2026-07-15 03:15:56 -0500

    Heat Generator
    A more advanced script for running the Heat Generator, as apposed to the simple looping power 0 to 10 every time efficiency drops below 100.

---
+++ Atmosphere/temp_heater_1.py
@@ -0,0 +1,42 @@
+
+log_status = 0
+
+heater = get_component("heater_1")
+therm_state = []
+power_watts = []
+
+
+print(f"Thermal_State at launch: {heater.thermal_state()}")
+
+while True:
+    
+    if heater.efficiency() != 100:
+
+        if heater.thermal_state() in therm_state:
+            heater.set_power(power_watts[therm_state.index(heater.thermal_state())])
+
+        if not therm_state or heater.thermal_state() not in therm_state:
+            therm_state.append(heater.thermal_state())
+        
+        
+        while power_watts.length < therm_state.length:
+            power_watts.append(None)
+                    
+            for j in range(11):
+                heater.set_power(j)
+                replaceable = heater.thermal_state()
+                replacer = therm_state.index(replaceable)
+                power_watts[replacer] = j
+
+                if log_status:
+                    print(f"therm_state= {heater.thermal_state()} Power= ({j})watts Eff= {heater.efficiency()}")
+
+                if heater.efficiency() == 100:
+                    break
+            if heater.efficiency() == 100:
+                break
+
+
+        if log_status:
+            print(f"--------\nThermal_State({heater.thermal_state()}) set to optimal Power({power_watts[therm_state.index(heater.thermal_state())]})watts Eff= {heater.efficiency()} \nHeater Output: {heater.output()}\n--------")
+
